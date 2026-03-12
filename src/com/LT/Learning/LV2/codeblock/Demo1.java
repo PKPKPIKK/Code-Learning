9a29339292826b6b805b31ea064ecc1004fab55f
PKPKPIK <pkpkpik@outlook.com>
2026-03-12 17:59:47 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/leetcode/editor.xml

---
+++ src/com/LT/Learning/LV2/codeblock/Demo1.java
@@ -0,0 +1,9 @@
+package com.LT.Learning.LV2.codeblock;
+
+public class Demo1 {
+    public static void main(String[] args) {
+//        int a = 10;
+//        System.out.println(a);
+        Student s = new Student();
+    }
+}
---
+++ src/com/LT/Learning/LV2/codeblock/Demo2.java
@@ -0,0 +1,8 @@
+package com.LT.Learning.LV2.codeblock;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        Student s1 = new Student("小王",18);
+    }
+}
